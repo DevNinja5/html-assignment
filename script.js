9545d13f82bd5708ff52d099942d7c6e052d6357
Rahul Soni <rahul.soni@knoldus.com>
2021-08-24 00:32:10 +0530

    added a redirection in script.js

--- script.js
+++ script.js
@@ -3,5 +3,7 @@ function validateForm() {
     fname = fname.trim();
     if(fname == ""){
         alert("*Name field is required*");
-    }
+        document.getElementById("fname_input").value="";
+        window.location.href="index.html";
+    } 
 }
